4f8749e07146659e8ede76c4a01fd74e33625cbf
NatureGeorge <414731811@qq.com>
2025-01-13 19:10:23 +0800

    fix ta tb pa pb error

--- pykorp/io.py
+++ pykorp/io.py
@@ -2,12 +2,37 @@
 # @Filename: io.py
 # @Email:  zhuzefeng@stu.pku.edu.cn
 # @Author: Zefeng Zhu
-# @Last Modified: 2025-01-11 12:29:45 pm
+# @Last Modified: 2025-01-13 07:09:12 pm
 import math
 import torch
 import roma
 from warnings import warn
+from typing import Optional
 from .utils import planar_angle, dihedral, projection
+
+
+aa_dict = dict(
+    ALA =  0,
+    CYS =  1,
+    ASP =  2,
+    GLU =  3,
+    PHE =  4,
+    GLY =  5,
+    HIS =  6,
+    ILE =  7,
+    LYS =  8,
+    LEU =  9,
+    MET =  10,
+    ASN =  11,
+    PRO =  12,
+    GLN =  13,
+    ARG =  14,
+    SER =  15,
+    THR =  16,
+    VAL =  17,
+    TRP =  18,
+    TYR =  19,
+)
 
 
 def frame_coords(n_coords: torch.Tensor, ca_coords: torch.Tensor, c_coords: torch.Tensor):
@@ -55,6 +80,8 @@ def featurize_frames(fa_v: torch.Tensor, fa_p: torch.Tensor, fb_v: torch.Tensor,
     
     Output shape: (...xaxbx6)
     '''
+    is_symmetric = (fa_v is fb_v) and (fa_p is fb_p)
+
     rab = fb_p.unsqueeze(-3) - fa_p.unsqueeze(-2) # shape: (...xaxbx3)
     rba = -rab
     
@@ -62,23 +89,41 @@ def featurize_frames(fa_v: torch.Tensor, fa_p: torch.Tensor, fb_v: torch.Tensor,
     # dmask = dab < cutoff
     
     fa_vx = fa_v[..., 0].unsqueeze(-2) # shape: (...xax1x3)
+    fa_vy = fa_v[..., 1].unsqueeze(-2) # shape: (...xax1x3)
     fa_vz = fa_v[..., 2].unsqueeze(-2) # shape: (...xax1x3)
-    fb_vx = fb_v[..., 0].unsqueeze(-3) # shape: (...x1xbx3)
-    fb_vz = fb_v[..., 2].unsqueeze(-3) # shape: (...x1xbx3)
 
     ta = planar_angle(fa_vz, rab)
-    tb = planar_angle(fb_vz, rba)
 
-    pa = planar_angle(fa_vx, rab - projection(rab, fa_vz)) # project on the z axis and then substract it
-    pb = planar_angle(fb_vx, rba - projection(rba, fb_vz)) # project on the z axis and then substract it
+    rab_on_axy = rab - projection(rab, fa_vz) # project on the z axis and then substract it
+    pa = torch.pi - planar_angle(fa_vx, rab_on_axy)
+    keep_maska = torch.einsum('...km,...km->...k', fa_vy, rab_on_axy) < 0
+    pa[~keep_maska] *= -1
+    pa += torch.pi
 
-    chi = torch.pi + dihedral(-fa_vz, rab, fb_vz)
+    if is_symmetric:
+        tb = None # ta.transpose(-1, -2)
+        pb = None # pa.transpose(-1, -2)
+        fb_vz = fb_v[..., 2].unsqueeze(-3) # shape: (...x1xbx3)
+    else:
+        fb_vx = fb_v[..., 0].unsqueeze(-3) # shape: (...x1xbx3)
+        fb_vy = fb_v[..., 1].unsqueeze(-3) # shape: (...x1xbx3)
+        fb_vz = fb_v[..., 2].unsqueeze(-3) # shape: (...x1xbx3)
+
+        tb = planar_angle(fb_vz, rba)
+
+        rba_on_bxy = rba - projection(rba, fb_vz) # project on the z axis and then substract it
+        pb = torch.pi - planar_angle(fb_vx, rba_on_bxy)
+        keep_maskb = torch.einsum('...km,...km->...k', fb_vy, rba_on_bxy) < 0
+        pb[~keep_maskb] *= -1
+        pb += torch.pi
+
+    chi = torch.pi + dihedral(fa_vz, rba, -fb_vz)
     
     return dab, ta, tb, pa, pb, chi
 
 
 def discretize_features(br: torch.Tensor, theta: torch.Tensor, dpsi: torch.Tensor, dchi: float, nring: int, ncellsring: torch.Tensor,
-                        dab: torch.Tensor, ta: torch.Tensor, tb: torch.Tensor, pa: torch.Tensor, pb: torch.Tensor, chi: torch.Tensor):
+                        dab: torch.Tensor, ta: torch.Tensor, tb: Optional[torch.Tensor], pa: torch.Tensor, pb: Optional[torch.Tensor], chi: torch.Tensor):
     '''
     Discretize the 6D features into bin indices.
     
@@ -86,25 +131,31 @@ def discretize_features(br: torch.Tensor, theta: torch.Tensor, dpsi: torch.Tenso
     '''
     ir = torch.clip(torch.bucketize(dab, br, right=False) - 1, min=0)
     ita = torch.clip(torch.bucketize(ta, theta, right=False), min=0, max=nring-1)
-    itb = torch.clip(torch.bucketize(tb, theta, right=False), min=0, max=nring-1)
     ipa = (pa / dpsi[ita]).to(dtype=torch.int64)
-    ipb = (pb / dpsi[itb]).to(dtype=torch.int64)
     ic = (chi / dchi).to(dtype=torch.int64)
     
     nita = ncellsring[ita]
     ipa_mask = ipa >= nita
     if ipa_mask.any():
-        warn('ipa_mask'); ipa[ipa_mask] = nita[ipa_mask] - 1
+        warn(f'ipa_mask: {ipa_mask.sum()}'); ipa[ipa_mask] = nita[ipa_mask] - 1
     
-    nitb = ncellsring[itb]
-    ipb_mask = ipb >= nitb
-    if ipb_mask.any():
-        warn('ipb_mask'); ipb[ipb_mask] = nitb[ipb_mask] - 1
-    
+    if tb is None:
+        assert pb is None
+        itb = ita.transpose(-1, -2)
+        ipb = ipa.transpose(-1, -2)
+    else:
+        itb = torch.clip(torch.bucketize(tb, theta, right=False), min=0, max=nring-1)
+        ipb = (pb / dpsi[itb]).to(dtype=torch.int64)
+
+        nitb = ncellsring[itb]
+        ipb_mask = ipb >= nitb
+        if ipb_mask.any():
+            warn(f'ipb_mask: {ipb_mask.sum()}'); ipb[ipb_mask] = nitb[ipb_mask] - 1
+
     ic_thr = round(2 * math.pi / dchi)
     ic_mask = ic >= ic_thr
     if ic_mask.any():
-        warn('ic_mask'); ic[ic_mask] = ic_thr - 1
+        warn(f'ic_mask: {ic_mask.sum()}'); ic[ic_mask] = ic_thr - 1
 
     return ir, ita, itb, ipa, ipb, ic
 
@@ -137,7 +188,7 @@ def korp_energy(
     dab_min, dab_max = br[0], br[-1]
     mask = ((seqsepab > 0) & (dab > dab_min) & (dab < dab_max)).to(dtype=torch.int64) # shape (...xaxb)
     
-    sd = seqsepab.clone()     # | -> when `smapping=[ 0, -1,  1,  1,  1,  0,  0,  0,  0,  0]`, `bonding_thr` is essentially 4 and `seqsepab` should > 1
+    sd = seqsepab.abs()       # | -> when `smapping=[ 0, -1,  1,  1,  1,  0,  0,  0,  0,  0]`, `bonding_thr` is essentially 4 and `seqsepab` should > 1
     sd[sd > bonding_thr] = 0  # | -> thus `mask` should be `((seqsepab > 1) & (dab > dab_min) & (dab < dab_max)).to(dtype=torch.int64)`
     s = smapping[sd]          # | -> then `s` can be directly assigned to be `(seqsepab <= bonding_thr).to(dtype=torch.int64)` given that `bonding_thr = 4` # shape (...xaxb)
     mask &= s >= 0            # | -> but here remain the original code for potential flexibility
